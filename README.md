1eaa314d40e0ead67fa1d33e2341c338fc0343d5
Jiri Kratochvil <jiri.kratochvil@topmonks.com>
2015-09-21 17:10:51 +0200

    Scripts, cron, dockerized

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ export DOCKERBACKUP_RSYNC_USER=<rsync user>
 export DOCKERBACKUP_RSYNC_HOST=<rsync server hostname>
 export DOCKERBACKUP_NODE_NAME=<name of box>
 export DOCKERBACKUP_RSYNC_PASSWORD=<rsync password>
+export DOCKERBACKUP_FOLDER=<folder for backup>
 ``` 
 
 Run backup:
--- run_backup.sh
+++ run_backup.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
 RSYNC_USER="$DOCKERBACKUP_RSYNC_USER"
 RSYNC_HOST="$DOCKERBACKUP_RSYNC_HOST"
 SERVER_NAME="$DOCKERBACKUP_NODE_NAME"
 
 export RSYNC_PASSWORD="$DOCKERBACKUP_RSYNC_PASSWORD"
 
-echo "#####"
-echo ""
+echo "#####"  >> /var/log/cron.log 2>&1
+echo "Starting backup of $DOCKERBACKUP_FOLDER . . . " >> /var/log/cron.log 2>&1
+rsync -a --inplace $DOCKERBACKUP_FOLDER rsync://$RSYNC_USER@$RSYNC_HOST/$RSYNC_USER/$SERVER_NAME >> /var/log/cron.log 2>&1
 
-echo "Starting backup of $DOCKERBACKUP_FOLDER . . . "
-rsync -a --inplace $DOCKERBACKUP_FOLDER rsync://$RSYNC_USER@$RSYNC_HOST/$RSYNC_USER/$SERVER_NAME
-
-echo ""
-echo "####"
+echo "####" >> /var/log/cron.log 2>&1
